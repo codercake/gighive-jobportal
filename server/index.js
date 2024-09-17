5c53d9bc30e08c8ca33b348017b709734917a02c
codercake <ishithaac2312@gmail.com>
2024-09-17 16:25:40 +0530

    routes backend

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
-import express from 'express'; 
+import express from 'express';
 import cors from 'cors';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import authRoutes from './routes/auth.route.js';
 import jobRoutes from './routes/jobRoutes.js';
+import notificationRoutes from './routes/notificationRoutes.js'; 
+import { authenticateJWT } from './middlewares/authMiddleware.js'; 
+import setupSocket from './utils/socket.js'; 
 
 dotenv.config();
 
@@ -11,20 +14,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Register routes
-app.use('/auth', authRoutes);
-app.use('/jobs', jobRoutes);
-
-const port = process.env.PORT || 5001;
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-
-// MongoDB connection
+// MongoDB Connection
 mongoose.connect(process.env.RESTREVIEWS_DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000
 })
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err))
+.then(() => console.log('MongoDB Connected'))
+.catch((error) => {
+    console.error('Failed to connect to MongoDB', error);
+    process.exit(1);
+});
+
+// Register Routes
+app.use('/auth', authRoutes);
+app.use('/jobs', jobRoutes);
+app.use('/notifications', authenticateJWT, notificationRoutes); 
+
+app.get('/', (req, res) => res.send('Welcome to the Job Listing Portal!'));
+
+const server = app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server is running at port ${process.env.PORT || 5000}`);
+});
+
+const io = setupSocket(server); 
--- server/routes/notificationRoutes.js
+++ server/routes/notificationRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addNotification } from '../controllers/notificationController.js';
+import { addNotification } from '../controllers/notification.controller.js';
 
 const router = express.Router();
 router.post('/send', addNotification);
