263557d1309721fb0939478066f3e8c8508a54f4
codercake <ishithaac2312@gmail.com>
2024-09-17 16:34:05 +0530

    removed trailing whitespace

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; 
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
 
-ReactDOM.render(
-  <Router>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </Router>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
