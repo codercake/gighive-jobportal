d8e248c43d55cb02c535d3cb5df8087d0e22ca2b
codercake <ishithaac2312@gmail.com>
2024-09-19 20:40:45 +0530

    server configuration

--- server/index.js
+++ server/index.js
@@ -11,7 +11,9 @@ import setupSocket from './utils/socket.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000', 
+}));
 app.use(express.json());
 
 // MongoDB Connection
