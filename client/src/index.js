68e6a1b5d139af10948b4077e411d553fd645b93
codercake <ishithaac2312@gmail.com>
2024-09-28 21:22:22 +0530

    dependencies modification

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from 'react-dom/client'; 
+import App from './App.jsx'; 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+root.render(<App />);
